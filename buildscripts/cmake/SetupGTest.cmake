11a7eb921fe43b83d8c6cbc302c28479996a78f1
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-11-30 14:08:07 +0100

    fix: MUSE_FRAMEWORK_SRC_PATH -> MUSE_FRAMEWORK_PATH

--- buildscripts/cmake/SetupGTest.cmake
+++ buildscripts/cmake/SetupGTest.cmake
@@ -18,4 +18,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-include(${MUSE_FRAMEWORK_SRC_PATH}/testing/gtest.cmake)
+include(${MUSE_FRAMEWORK_PATH}/testing/gtest.cmake)
